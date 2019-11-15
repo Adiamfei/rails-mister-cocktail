e0ad463d5a7400533e62bf34c4dd4199fd87cc43
adiam feireiss <adiamfeireiss@gmail.com>
2019-11-15 16:56:36 +0100

    sadiefix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleansing the database'
+Ingredient.destroy_all
+Dose.destroy_all
+Cocktail.destroy_all
+
+puts 'Creating Ingredients ...'
+Ingredient.create(name: "Lemon")
+Ingredient.create(name: "Ice")
+Ingredient.create(name: "Mint Leaves")
+Ingredient.create(name: "Spiced Rum")
+Ingredient.create(name: "Applejack")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Dark Rum")
+Ingredient.create(name: "Pineapple Juice")
+Ingredient.create(name: "Strawberries")
+Ingredient.create(name: "Rasberries")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Tonic Water")
+Ingredient.create(name: "Grenedine")
+Ingredient.create(name: "White Rum")
+Ingredient.create(name: "Tomato Juice")
+Ingredient.create(name: "Lime")
+Ingredient.create(name: "Orange Juice")
+Ingredient.create(name: "Cranberry Juice")
+Ingredient.create(name: "Perseco")
+Ingredient.create(name: "Brown Sugar")
+
+puts 'Creating Cocktails ...'
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Sex on the Beach")
+Cocktail.create(name: "Cosmopolitan")
+Cocktail.create(name: "Screwdriver")
+Cocktail.create(name: "Vodka Tonic")
+Cocktail.create(name: "Gin Tonic")
+Cocktail.create(name: "Mai Tai")
+Cocktail.create(name: "French 75")
+Cocktail.create(name: "Rum and Coke")
+Cocktail.create(name: "Moscow Mule")
+Cocktail.create(name: "Blue Swimming Pool")
+Cocktail.create(name: "Momosa")
+Cocktail.create(name: "Long Island IceTea")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Tim Collins")
+Cocktail.create(name: "Whisky Sour")
+Cocktail.create(name: "Cuba Libre")
+Cocktail.create(name: "Zombie")
+Cocktail.create(name: "Tequilla Slammer")
+Cocktail.create(name: "Mud Slide")
+
+puts 'Finished!'
